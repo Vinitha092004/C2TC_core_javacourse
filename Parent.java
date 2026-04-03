b11c30a78a1a9fb3a85a52167e14953b1bb9510a
Vinitha092004 <vinithamurali48@gmail.com>
2026-04-03 09:45:20 +0530

    Add files via upload

---
+++ Parent.java
@@ -0,0 +1,13 @@
+package com.tnsif.accessmodifiers;
+
+public class Parent {
+	// PROTECTED: Only the subclasses or in the same package
+	
+	protected String secretReceipe = "Grandma's Secret Spices";
+	
+	protected void cook() {
+		System.out.println("Cooking with" + secretReceipe);
+		
+	}
+
+}
